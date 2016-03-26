bc0f44d10235792123aa7fccd56bcd4c3458a5d3
Anna Taberski <aftaberski@gmail.com>
2016-03-26 12:02:53 -0400

    Add function to symmetrize body parts of hobbit

--- hobbit.clj
+++ hobbit.clj
@@ -8,3 +8,21 @@
    {:name "left-ear" :size 1}
    {:name "mouth" :size 1}
    {:name "nose" :size 1}])
+
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+
+(defn symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts
+         final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+               (into final-body-parts
+                     (set [part (matching-part part)])))))))
