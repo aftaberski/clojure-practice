4b03d573837d77acc9b0624777880c0d7fff669f
Anna Taberski <aftaberski@gmail.com>
2016-03-26 11:46:41 -0400

    Add hobbit body parts

---
+++ hobbit.clj
@@ -0,0 +1,10 @@
+(defn test
+  []
+  println "Test hobbit")
+
+(def hobbit-body-parts 
+  [{:name "head" :size 3}
+   {:name "left-eye" :size 1}
+   {:name "left-ear" :size 1}
+   {:name "mouth" :size 1}
+   {:name "nose" :size 1}])
