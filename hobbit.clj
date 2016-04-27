e1604d39044b117aa00b261fa4f006ca8cad5d37
Anna Taberski <aftaberski@gmail.com>
2016-04-26 20:48:33 -0400

    Add function that picks a hobbit body part to hit....)

--- hobbit.clj
+++ hobbit.clj
@@ -1,3 +1,6 @@
+(ns hobbit-stuff
+  (:gen-class))
+
 (defn test
   []
   println "Test hobbit")
@@ -30,7 +33,31 @@
 (def list-of-foods-i-am-eating-right-now
   ["Bananas" "Crakers" "Peanut Butter"])
 
-(defn print-all-of-the-foodz 
-  []
-  (loop [food list-of-foods-i-am-eating-right-now]
-    (println food)))
+(defn recursive-printer
+  ([]
+     (recursive-printer 0))
+  ([iteration]
+     (println iteration)
+     (if (> iteration 3)
+       (println "Goodbye!")
+       (recursive-printer (inc iteration)))))
+(recursive-printer)
+
+(defn better-symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts))
+
+(defn hit
+  [asym-body-parts]
+  (let [sym-parts (better-symmetrize-body-parts asym-body-parts)
+        body-part-size-sum (reduce + (map :size sym-parts))
+        target (rand body-part-size-sum)]
+    (loop [[part & remaining] sym-parts
+           accumulated-size (:size part)]
+      (if (> accumulated-size target)
+        part
+        (recur remaining (+ accumulated-size (:size (first remaining))))))))
